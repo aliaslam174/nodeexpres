47f0b3543416f6b766fa682bf1ca46da83c4a2df
Muhammad Ali <122381911+aliaslam174@users.noreply.github.com>
2024-02-26 11:33:01 +0500

    update

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const mongoose=require('mongoose')
+
+const port =  3000;
+
+// Define a route
+app.get('/', (req, res) => {
+  res.send('my nodeexpress project!');
+});
+
+// Start the server
+mongoose.connect('mongodb+srv://aliaslam22246:4YqMspL89JWB9k50@cluster0.ia8nqr9.mongodb.net/blog').then(()=>{
+    app.listen(port,()=>{
+        console.log(`server is listen ${port}`)
+    })
+
+})
